d475609bd1efb00550dd894158ea2c354c1e2ff5
Mathias Tang <tilmathiastang@gmail.com>
2022-07-10 18:03:16 +0200

    missing files

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+  eleventyConfig.addWatchTarget("src/css/");
+
+
+
+  eleventyConfig.addCollection('posts', function(collectionApi) {
+    return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md');
+  })
+
+
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      output: 'public',
+    },
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+  };
+}
