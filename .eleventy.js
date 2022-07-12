69ec6cbd751e2754974a3f95b042f9e63f7ffefb
Mathias Tang <tilmathiastang@gmail.com>
2022-07-12 13:08:05 +0200

    small changes to posts

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,11 @@ module.exports = function(eleventyConfig) {
     return collection.getAll().filter((item) => item.data.customKey);
   });
 
+  const embedEverything = require("eleventy-plugin-embed-everything");
+  module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(embedEverything);
+};
+
 
   return {
     dir: {
---
+++ src/blog/posts/t-wraps feather/t-wrap-original-01.md
@@ -0,0 +1,9 @@
+---
+layout: layouts/post.html
+tags: original
+title: T-wrap 1
+images: []
+featuredimage: assets/images/original/t-wrap1_1.jpg
+permalink: "/{{ title | slugify }}/"
+description: T-wrap feather is packed by 10 pieces of the same design.
+---
