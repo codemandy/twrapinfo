8cf8ad7988ebf5c28c4ffe8a6392915da8bd0b42
Mathias Tang <tilmathiastang@gmail.com>
2022-07-12 22:20:02 +0200

    snipcart

--- .eleventy.js
+++ .eleventy.js
@@ -28,10 +28,6 @@ module.exports = function(eleventyConfig) {
     }
   });
 
-  eleventyConfig.addFilter("money", function (value) {
-    const formatter = new Intl.NumberFormat('en-US', { currency: 'USD', style: 'currency' })
-    return formatter.format(value)
-  })
 
   return {
     dir: {
